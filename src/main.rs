28d8b8e50626c7569ef297ca1c2e682a7a86fc7e
Riley Velasco <99685899+RileyVFineWorks@users.noreply.github.com>
2024-07-09 17:23:57 -0700

    update commands real quick

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 use std::env;
-use std::path::Path;
+use rustyline::error::ReadlineError;
 use rustyline::Editor;
 
 mod commands;
 mod utils;
 
 fn main() {
-    let mut rl = Editor::<()>::new().unwrap();
+    let mut rl = Editor::<()>::new();
+    if let Err(err) = rl {
+        match err {
+            ReadlineError::Io(err) => {
+                eprintln!("Failed to create readline editor: {}", err);
+                std::process::exit(1);
+            }
+            ReadlineError::Eof => {
+                eprintln!("Reached end of file");
+                std::process::exit(0);
+            }
+            ReadlineError::Interrupted => {
+                eprintln!("Interrupted");
+                std::process::exit(1);
+            }
+            _ => {
+                eprintln!("Unknown readline error");
+                std::process::exit(1);
+            }
+        }
+    }
+    let mut rl = rl.unwrap();
     let history_file = utils::get_history_file_path();
     if rl.load_history(&history_file).is_err() {
         println!("No previous history.");
